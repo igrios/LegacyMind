314eaa8c05a9cbf680f0323eca9f8616fe76f622
Ignacio Rios <ignarios1@hotmail.com>
2026-04-18 09:13:29 -0300

    Initial project structure with hexagonal architecture

---
+++ src/main/java/com/ignacio/legacyanalyzer/LegacyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ignacio.legacyanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignacio/legacyanalyzer/LegacyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignacio.legacyanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
