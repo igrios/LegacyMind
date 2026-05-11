094fc4208f3e13dcbdac785f261a4745c5d81c4b
Ignacio Rios <ignarios1@hotmail.com>
2026-05-11 17:29:06 -0300

    improve frontend layout and sidebar ui v6

--- frontend/src/components/GraphView.jsx
+++ frontend/src/components/GraphView.jsx
@@ -1,4 +1,8 @@
-import ReactFlow from "reactflow";
+import ReactFlow, {
+  Background,
+  Controls,
+  MiniMap
+} from "reactflow";
 
 import "reactflow/dist/style.css";
 
@@ -17,7 +21,8 @@ export default function GraphView({
     <div
       style={{
         width: "100%",
-        height: "100vh"
+        height: "100vh",
+        background: "#0f172a"
       }}
     >
 
@@ -34,7 +39,15 @@ export default function GraphView({
           onNodeClick(node.id);
         }}
 
-      />
+      >
+
+        <MiniMap />
+
+        <Controls />
+
+        <Background />
+
+      </ReactFlow>
 
     </div>
   );
