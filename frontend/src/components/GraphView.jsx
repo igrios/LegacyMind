03aeeb766d2d829eb3474e3f5690424ff12e69cd
Ignacio Rios <ignarios1@hotmail.com>
2026-05-11 15:49:52 -0300

    improve frontend layout sidepanel

--- frontend/src/components/GraphView.jsx
+++ frontend/src/components/GraphView.jsx
@@ -14,12 +14,12 @@ export default function GraphView({
 
   return (
 
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh"
-      }}
-    >
+   <div
+  style={{
+    width: "100%",
+    height: "100%"
+  }}
+>
 
       <ReactFlow
 
