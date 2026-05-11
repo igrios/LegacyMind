06f4752b828c99849595ba4b601ff1538081bd1f
Ignacio Rios <ignarios1@hotmail.com>
2026-05-11 17:15:29 -0300

    improve frontend layout and sidebar ui v3

--- frontend/src/components/GraphView.jsx
+++ frontend/src/components/GraphView.jsx
@@ -14,12 +14,12 @@ export default function GraphView({
 
   return (
 
-   <div
-  style={{
-    width: "100%",
-    height: "100%"
-  }}
->
+    <div
+      style={{
+        width: "100%",
+        height: "100vh"
+      }}
+    >
 
       <ReactFlow
 
