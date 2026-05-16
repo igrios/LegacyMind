9ec466da35d2c53ae63e594332b71e2380a333e5
Ignacio Rios <ignarios1@hotmail.com>
2026-05-16 13:23:16 -0300

    fix: add transactional support to database cleanup

--- src/main/java/com/ignacio/legacyanalyzer/application/usecase/DeleteDatabaseUseCase.java
+++ src/main/java/com/ignacio/legacyanalyzer/application/usecase/DeleteDatabaseUseCase.java
@@ -1,6 +1,7 @@
 package com.ignacio.legacyanalyzer.application.usecase;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.ignacio.legacyanalyzer.domain.ports.TableDependencyRepositoryPort;
 import lombok.RequiredArgsConstructor;
 
@@ -10,6 +11,7 @@ public class DeleteDatabaseUseCase {
 
     private final TableDependencyRepositoryPort repository;
 
+    @Transactional
     public void execute() {
 
         repository.deleteAll();
