7ad509df12d08ddb7fd27f72258f50a4641e77bf
Ignacio Rios <ignarios1@hotmail.com>
2026-05-20 11:52:02 -0300

    CORS backen controller

--- src/main/java/com/ignacio/legacyanalyzer/infrastructure/adapters/input/controller/HealthController.java
+++ src/main/java/com/ignacio/legacyanalyzer/infrastructure/adapters/input/controller/HealthController.java
@@ -4,12 +4,14 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/system")
+@CrossOrigin(origins = "*")
 public class HealthController {
 
     @GetMapping("/health")
